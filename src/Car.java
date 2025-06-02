48ea54b52d1079cdf906794b23ed0e0fcb27b6fc
Aslı Nur <aslinurmavili16500@gmail.com>
2025-06-02 12:33:53 +0300

    polymorphic

--- src/Car.java
+++ src/Car.java
@@ -1,51 +1,57 @@
-public class Car extends Vehicle {
+public class Car {
     private String brand;
     private String model;
     private int year;
+    private int speed;
 
-    // Constructor
     public Car(String brand, String model, int year) {
         this.brand = brand;
         this.model = model;
         this.year = year;
     }
 
-    // Getter metodları
+    // Getter ve Setter
     public String getBrand() {
         return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
     public String getModel() {
         return model;
     }
 
-    public int getYear() {
-        return year;
-    }
-
-    // Setter metodları
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public int getYear() {
+        return year;
     }
 
     public void setYear(int year) {
         this.year = year;
     }
 
-    // Bilgileri ekrana yazdırma
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
     public void printInfo() {
         System.out.println("Marka: " + brand);
         System.out.println("Model: " + model);
         System.out.println("Yıl: " + year);
-        System.out.println("Hız: " + getSpeed() + " km/s");
+        System.out.println("Hız: " + speed + " km/s");
     }
-    // Override edilmiş start() metodu
-    @Override
+
+    // Polymorphic metod
     public void start() {
-        System.out.println("Araba çalıştı, sürüşe hazır!");
+        System.out.println("Araba çalışıyor...");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2020);
+        // Polymorphism ile Car türünde ama ElectricCar nesnesi
+        Car myCar = new ElectricCar("Tesla", "Model 3", 2023);
         myCar.setSpeed(120);
 
         System.out.println("İlk Araç Bilgileri:");
         myCar.printInfo();
 
-        // start metodunu çağıralım (override edilmiş olan)
+        // start metodu override edildiği için ElectricCar versiyonu çalışır
         myCar.start();
 
-        myCar.setBrand("Honda");
-        myCar.setModel("Civic");
+        // Değerleri güncelle
+        myCar.setBrand("Tesla");
+        myCar.setModel("Model Y");
         myCar.setYear(2024);
         myCar.setSpeed(150);
 
         System.out.println("\nGüncellenmiş Araç Bilgileri:");
         myCar.printInfo();
 
-        // tekrar start() metodunu çağıralım
         myCar.start();
+
+        //benzinli araba
+        System.out.println("\n---");
+
+        Car gasCar = new GasCar("Toyota", "Corolla", 2020);
+        gasCar.setSpeed(130);
+
+        System.out.println("Benzinli Araç Bilgileri:");
+        gasCar.printInfo();
+        gasCar.start();
     }
 }
